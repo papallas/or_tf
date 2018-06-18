466d8b8f7a65a59b27b9b16ac40a60cc00970777
Rafael Papallas <sc13rp@leeds.ac.uk>
2018-06-18 16:49:03 +0100

    Fixed a bug where if relative transform was not provided would not accept plannar tracking.

--- pythonsrc/or_tf/or_tf.py
+++ pythonsrc/or_tf/or_tf.py
@@ -15,21 +15,31 @@ class OpenRAVE_TF:
         self.or_tf = orpy.RaveCreateSensor(self.env,'or_tf or_tf '+self.world_tf_id)
         env.Add(self.or_tf)
 
-    def RegisterBody(self, or_body, tf_id, openrave_frame_in_tf_frame=None, planar_tracking=False, fixed_translation_z=None):
-        if openrave_frame_in_tf_frame is None:
-            self.or_tf.SendCommand('RegisterBody ' + or_body.GetName() + ' ' + tf_id)
-        else:
+    def RegisterBody(self, or_body, tf_id, openrave_frame_in_tf_frame=None, planar_tracking=False, fixed_translation_z=-1.0):
+        x = 0.0
+        y = 0.0
+        z = 0.0
+        qw = 1.0
+        qx = 0.0
+        qy = 0.0
+        qz = 0.0
+
+        if openrave_frame_in_tf_frame is not None:
             x = openrave_frame_in_tf_frame[0,3]
             y = openrave_frame_in_tf_frame[1,3]
             z = openrave_frame_in_tf_frame[2,3]
             quat = orpy.quatFromRotationMatrix(openrave_frame_in_tf_frame[:3,:3])
+            qw = quat[0]
+            qx = quat[1]
+            qy = quat[2]
+            qz = quat[3]
 
-            cmd = 'RegisterBody ' + or_body.GetName() + ' ' + tf_id + ' openrave_frame_in_tf_frame ' + str(x) + ' ' + str(y) + ' ' + str(z) + ' ' + str(quat[0]) + ' ' + str(quat[1]) + ' ' + str(quat[2]) + ' ' + str(quat[3])
-            if planar_tracking:
-                if fixed_translation_z is None:
-                    fixed_translation_z = -1.0
-                cmd = cmd + ' planar_tracking' + ' ' + 'fixed_translation_z' + ' ' + str(fixed_translation_z)
-            self.or_tf.SendCommand(cmd)
+        cmd = 'RegisterBody ' + or_body.GetName() + ' ' + tf_id + ' openrave_frame_in_tf_frame ' + str(x) + ' ' + str(y) + ' ' + str(z) + ' ' + str(qw) + ' ' + str(qx) + ' ' + str(qy) + ' ' + str(qz)
+
+        if planar_tracking:
+            cmd +=' planar_tracking' + ' ' + 'fixed_translation_z' + ' ' + str(fixed_translation_z)
+
+        self.or_tf.SendCommand(cmd)
 
     def RegisterRobotHand(self, or_body, tf_id):
         self.or_tf.SendCommand('RegisterRobotHand ' + or_body.GetName() + ' ' + tf_id)
